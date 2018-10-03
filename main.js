01c4148cfae187a47b778c9b33f5fb7c236a9e0d
Marijana Zlatkovic <m.zlatkovic@ymail.com>
2018-10-03 16:18:16 +0200

    select page elements

---
+++ main.js
@@ -0,0 +1,9 @@
+// Page elements
+const $input = $('#dreamLand');
+const $submit = $('#go');
+const $container = $('.container');
+const $destination = $('#destination');
+const $venueDivs = [$('#venue1'), $('#venue2'), $('#venue3'), $('#venue4')];
+const $weatherDivs = [$('#weather1'), $('#weather2'), $('#weather3'), $('#weather4')];
+
+const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
