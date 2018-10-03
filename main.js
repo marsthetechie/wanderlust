4741ca431b994014863e2300395874be5fe1f13e
Marijana Zlatkovic <m.zlatkovic@ymail.com>
2018-10-03 18:41:29 +0200

    empty divs

--- main.js
+++ main.js
@@ -7,3 +7,16 @@ const $venueDivs = [$('#venue1'), $('#venue2'), $('#venue3'), $('#venue4')];
 const $weatherDivs = [$('#weather1'), $('#weather2'), $('#weather3'), $('#weather4')];
 
 const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+const searchDestination = () => {
+    // Empty divs on new input
+    $venueDivs.forEach(venue => {
+        return venue.empty();
+    });
+    $weatherDivs.forEach(day => {
+        return day.empty();
+    });
+    $destination.empty();
+}
+
+$submit.click(searchDestination);
