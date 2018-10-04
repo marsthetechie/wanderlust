1565674f61878d5bb933365cc29eacae91bc3409
Marijana Zlatkovic <m.zlatkovic@ymail.com>
2018-10-04 17:50:35 +0200

    add a function to get venues

--- main.js
+++ main.js
@@ -8,6 +8,31 @@ const $weatherDivs = [$('#weather1'), $('#weather2'), $('#weather3'), $('#weathe
 
 const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
+const clientId;
+const clientSecret;
+const url = 'https://api.foursquare.com/v2/venues/explore?';
+
+const getVenues = async () => {
+    let limit = 10;
+    const city = $input.val();
+    const urlToFetch = `${url}client_id=${clientId}&client_secret=${clientSecret}&v=20181004&limit=${limit}&near=${city}`;
+
+    try {
+        const response = await fetch(urlToFetch);
+        if (response.ok) {
+            const jsonResponse = response.json();
+            const venues = jsonResponse.response.groups[0].items.map(item => {
+                return item.venue;
+            })
+            return venues;
+        } else {
+            throw new Error('Failed!');
+        }
+    } catch (error) {
+        alert(error.message);
+    }
+}
+
 const searchDestination = () => {
     // Empty divs on new input
     $venueDivs.forEach(venue => {
@@ -17,6 +42,7 @@ const searchDestination = () => {
         return day.empty();
     });
     $destination.empty();
+    $container.css("visibiity", "visible");
 }
 
 $submit.click(searchDestination);
